31c23af2941a352e4477c978e96186f00ad4435f
cyberbuddy-manas <rspmanas1@gmail.com>
2023-03-05 13:51:36 +0530

    adding gitignore and removing comment

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.post('/sendMessage', async (req, res) => {
+app.post('/sendMessage', async (req, res) => {
 
-//     console.log(req.body);
+    console.log(req.body);
 
-//     const response = await fast2sms.sendMessage({
-//         authorization: "bMvQ4fxGCVqBTJ0KFP16glLA3ikN8mRytZwYS9XzuW5pdDo7n2GHYc8tkzLbhCP2MaIoqA5XOKFx07Re",
-//         message: req.body.message,
-//         numbers: [req.body.number]
-//     })
+    const response = await fast2sms.sendMessage({
+        authorization: "bMvQ4fxGCVqBTJ0KFP16glLA3ikN8mRytZwYS9XzuW5pdDo7n2GHYc8tkzLbhCP2MaIoqA5XOKFx07Re",
+        message: req.body.message,
+        numbers: [req.body.number]
+    })
 
-//     res.status(201).send({"message": response});
-// });
+    res.status(201).send({"message": response});
+});
 
 app.get('/', (req, res) => {
     res.send({message: "welcome"})
